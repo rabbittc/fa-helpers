393180b31b84c88b89d8735d7e67c08b57c8b1d9
Yuom Theara <yuom.theara@gmail>
2015-04-22 16:41:39 +0700

    update package.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
     name: 'theara:fa-helpers',
-    version: '0.0.1',
+    version: '0.0.2',
     // Brief, one-line summary of the package.
     summary: 'Font Awesome helper',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/rabbittc/fa-helpers.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
